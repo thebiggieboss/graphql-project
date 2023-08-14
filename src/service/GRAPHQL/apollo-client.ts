85379aab7e1006d0123b9373fb22e3a91f3c876a
headhoncho <dauitovarman@gmail.com>
2023-08-14 08:58:45 +0600

    fix(token) fix github token

--- src/service/GRAPHQL/apollo-client.ts
+++ src/service/GRAPHQL/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core
 const httpLink = createHttpLink({
     uri: 'https://api.github.com/graphql',
     headers: {
-        Authorization: `Bearer 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`
+        Authorization: `Bearer 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`
     }
 });
 
