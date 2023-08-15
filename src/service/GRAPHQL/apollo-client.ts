8f235f2134159789b555762e94aaa86bdf8e9d03
thebiggieboss <thebigboss@internet.ru>
2023-08-15 18:07:11 +0600

    dot env

--- src/service/GRAPHQL/apollo-client.ts
+++ src/service/GRAPHQL/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core
 const httpLink = createHttpLink({
     uri: 'https://api.github.com/graphql',
     headers: {
-        Authorization: `Bearer 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`
+        Authorization: `Bearer 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`
     }
 });
 
